43a7cefb0f450c98e6d027e7dc4e12c6dce0ae05
khuonghoanghuy <khuonghoanghuy@gmail.com>
2024-12-18 10:57:36 +0700

    added some more thing

---
+++ source/Conductor.hx
@@ -0,0 +1,13 @@
+package;
+
+typedef BPMChangeEvent =
+{
+	var stepTime:Int;
+	var songTime:Float;
+	var bpm:Int;
+}
+
+class Conductor
+{
+	public static var bpm:Int = 100;
+}
--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -5,4 +5,7 @@ import flixel.addons.ui.FlxUIState;
 class MusicBeatState extends FlxUIState
 {
 	var controls:Controls;
+	var curBeat:Int = 0;
+	var curStep:Int = 0;
+	var curSection:Int = 0;
 }
