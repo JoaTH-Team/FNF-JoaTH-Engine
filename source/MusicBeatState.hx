4ace31e5e6d4bc02a3f235e8fa16d65f58e1ae46
Joalor64 <rhgarcon@gmail.com>
2024-12-18 07:43:36 -0500

    did curbeat and curstep stuff also made a very barebones conductor

--- source/MusicBeatState.hx
+++ source/MusicBeatState.hx
@@ -8,4 +8,40 @@ class MusicBeatState extends FlxUIState
 	var curBeat:Int = 0;
 	var curStep:Int = 0;
 	var curSection:Int = 0;
+
+	override function create()
+	{
+		super.create();
+	}
+
+	override function update(elapsed:Float)
+	{
+		var oldStep:Int = curStep;
+
+		updateCurStep();
+		updateCurBeat();
+
+		if (oldStep != curStep && curStep > 0)
+			stepHit();
+
+		super.update(elapsed);
+	}
+
+	private function updateBeat():Void
+    {
+        curBeat = Math.floor(curStep / 4);
+    }
+
+    private function updateCurStep():Void
+    {
+        curStep = Math.floor(Conductor.songPosition / Conductor.stepCrochet);
+    }
+
+    public function stepHit():Void
+    {
+        if (curStep % 4 == 0)
+            beatHit();
+    }
+
+    public function beatHit():Void {}
 }
